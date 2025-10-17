581999393aca9d7331e6bcffe4241e7b637b5089
Gokulk-306 <ceecgokul2024@gmail.com>
2025-10-17 20:27:01 +0530

    feat: variables.tf added

---
+++ terraform-sg/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-2"
+  description = "AWS region for deployment"
+}
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for EC2"
+  default     = "ami-0cfde0ea8edd312d4"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  description = "Instance type for EC2"
+}
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID for the EC2 instance"
+}
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the security group"
+}
+variable "sg_name" {
+  type    = string
+  default = "demo_sg"
+}
+variable "instance_name" {
+  type    = string
+  default = "Web-Server"
+}
