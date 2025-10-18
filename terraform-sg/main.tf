101e2d44ef5adb33396bbb15218016ec8045da81
Gokulk-306 <ceecgokul2024@gmail.com>
2025-10-18 12:33:15 +0530

    Feat: Updated main.tf

--- terraform-sg/main.tf
+++ terraform-sg/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.aws_region
 }
+
 
 data "aws_vpc" "default" {
   default = true
@@ -12,6 +13,7 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
+
 
 resource "aws_security_group" "demo_sg" {
   name        = "demo-sg"
@@ -50,31 +52,43 @@ resource "aws_security_group" "demo_sg" {
   }
 
   tags = {
-    Name = "demo-SG"
+    Name = "demo-sg"
   }
 }
 
 resource "aws_instance" "web" {
   count                       = 2
-  ami                         = "ami-0cfde0ea8edd312d4" # Ubuntu 22.04 (ohio)
-  instance_type               = "t3.micro"
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
   subnet_id                   = element(data.aws_subnets.default.ids, count.index)
   vpc_security_group_ids      = [aws_security_group.demo_sg.id]
   associate_public_ip_address = true
+  key_name                    = var.key_name
 
+ 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install nginx -y",
+      "echo '<h1>Hello from Web Server ${count.index + 1}</h1>' | sudo tee /var/www/html/index.html",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/mnt/c/Users/Gokul/Downloads/IGW.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "echo EC2 instance ${count.index + 1} created and configured >> instance_log.txt"
+  }
 
   tags = {
     Name = "Web-Server-${count.index + 1}"
     Env  = "Dev"
   }
-# EOF is called "heredoc"
-  user_data = <<-EOF
-     #!/bin/bash
-    sudo apt update -y
-    sudo apt install nginx -y
-    echo "<h1>Hello from Web Server ${count.index + 1}</h1>" > /usr/share/nginx/html/index.html
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-    EOF
-
 }
